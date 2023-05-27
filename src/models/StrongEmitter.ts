b68aed146d862c981d900856078de64f7fbf4162
Davide Taurisano <davidetaurisano@MacBook-Pro-di-Davide.local>
2023-05-27 15:04:06 +0200

    feat: once integrated

--- src/models/StrongEmitter.ts
+++ src/models/StrongEmitter.ts
@@ -2,6 +2,7 @@ import { EmitterStructure, Listener } from "../types";
 
 export class StrongEmitter<T> implements EmitterStructure<T> {
   listeners: Listener<T>[] = [];
+  listenersOnce: Listener<T>[] = [];
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   private constructor() {}
@@ -9,11 +10,20 @@ export class StrongEmitter<T> implements EmitterStructure<T> {
   on(callback: Listener<T>) {
     this.listeners.push(callback);
   }
+
+  once(callback: Listener<T>) {
+    this.listenersOnce.push(callback);
+  }
+
   off(callback: Listener<T>) {
     this.listeners.splice(this.listeners.indexOf(callback), 1);
   }
   emit(payload: T) {
     this.listeners.forEach((listener) => listener(payload));
+    if (this.listenersOnce.length > 0) {
+      this.listenersOnce.forEach((listener) => listener(payload));
+      this.listenersOnce = [];
+    }
   }
   offAll() {
     this.listeners = [];
